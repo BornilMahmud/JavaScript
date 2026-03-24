b1a4aa1b43df7e49642e06988865718b06e92eee
Bornil Mahmud <bornilprof@gmail.com>
2026-03-24 20:32:38 +0600

    Push 4 file(s) via BM Compiler

---
+++ javaScript/begining js/name.js
@@ -0,0 +1,12 @@
+// practicing variable 
+let firstName = "bornil";
+let lastName = "mahmud";
+console.log(firstName+" "+lastName);
+
+const x = 20
+let y= 40
+y=70
+
+// x = 30 error because of const variable cannot be changeable ! 
+console.log (y,x)
+
